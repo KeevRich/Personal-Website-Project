4fb67732f07b893d4909b03d47d85e0978293749
Keevin Richards <keevinrichards@Keevins-MBP-2.lan>
2023-10-23 16:42:46 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// Get references to the navigation buttons
+const homeButton = document.getElementById('home-button');
+const aboutButton = document.getElementById('about-button');
+const portfolioButton = document.getElementById('portfolio-button');
+
+// Get references to the content sections
+const homeSection = document.getElementById('home-section');
+const aboutSection = document.getElementById('about-section');
+const portfolioSection = document.getElementById('portfolio-section');
+
+// Hide all sections except the home section initially
+aboutSection.style.display = 'none';
+portfolioSection.style.display = 'none';
+
+// Add event listeners to the buttons
+homeButton.addEventListener('click', () => {
+    homeSection.style.display = 'block';
+    aboutSection.style.display = 'none';
+    portfolioSection.style.display = 'none';
+});
+
+aboutButton.addEventListener('click', () => {
+    homeSection.style.display = 'none';
+    aboutSection.style.display = 'block';
+    portfolioSection.style.display = 'none';
+});
+
+portfolioButton.addEventListener('click', () => {
+    homeSection.style.display = 'none';
+    aboutSection.style.display = 'none';
+    portfolioSection.style.display = 'block';
+});
